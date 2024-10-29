0786221305c9ed8101b132ada8d710664b76b962
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-10-29 21:19:47 +0545

    Showed error in frontend

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,14 +3,22 @@ package main
 import (
 	"errors"
 	"fmt"
-
-	// "html/template"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
+
 	"snippitbox.chronoabi.com/internal/models"
 )
+
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
@@ -50,28 +58,73 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-  data := app.newTemplateDate(r)
+	data := app.newTemplateDate(r)
 
-  app.render(w, http.StatusOK,"create.tmpl.html",data)
-	// w.Write([]byte("Display the form for creating a new snippet..."))
+	// we set initial value form here
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
+	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
+
+// to display error we first created a snippetCreate Type now we will fill it with data I guess
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
-	// Create some variables holding dummy data. We'll remove these later on
-	// during the build.
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
+	err := r.ParseForm()
 
-	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// so we first take the expires value
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// then create a form variable which will be a snippetCreate structure
+
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
+
+	// Validation for Title
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "Title cannot be empty !"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "Title cannot be more then 100 characters"
+	}
+
+	//content cannot be empty
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "Content field cannot be empty !"
+	}
+
+	// expires time can only be 1, 7 or 365 days
+	if expires != 1 && expires != 7 && expires != 365 {
+		form.FieldErrors["expires"] = "Expiry days can only be 1, 7 or 365 days "
+
+	}
+	// render comes form the helper.go
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateDate(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
+		// fmt.Fprint(w, form.FieldErrors)
+		return
+	}
+
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	// w.Write([]byte("Creating snippits"))
 
-	http.Redirect(w, r, fmt.Sprintf("/snippets/view?%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
 }
-
---
+++ internal/validator/validator.go
@@ -0,0 +1,7 @@
+package validator
+
+import (
+  "strings"
+  "unicode/utf8"
+)
+
