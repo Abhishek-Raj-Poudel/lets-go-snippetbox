e33d0274b9d4e149b0585b9cab055e6fe175dfde
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-11-01 12:52:45 +0545

    Setting up the session manager

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,31 +7,33 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"snippitbox.chronoabi.com/internal/models"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
 
 	addr := flag.String("addr", ":4001", "HTTP network address")
-
 	dsn := flag.String("dsn", "web:root_123@/snippetbox?parseTime=true", "MySQL data Source name")
 
 	flag.Parse()
 
-	// too log error in more detail
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
@@ -47,14 +49,19 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
-  formDecoder := form.NewDecoder()
+	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
 
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
-    formDecoder: formDecoder,
+		formDecoder:   formDecoder,
+    sessionManager: sessionManager,
 	}
 
 	srv := &http.Server{
