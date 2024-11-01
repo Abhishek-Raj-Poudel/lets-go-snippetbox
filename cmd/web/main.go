c835ec409592d86a0975f5aad448686739e1b86d
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-11-01 19:07:38 +0545

    generated a self signed tls certificate and server is working as well

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -55,6 +55,8 @@ func main() {
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
 
+  sessionManager.Cookie.Secure = true
+
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
@@ -71,7 +73,7 @@ func main() {
 	}
 
 	infoLog.Printf("Server started in port %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem","./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
