366b3f0d0384307366025d5aeb00a85861b72208
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-11-01 11:47:55 +0545

    Now validator.go works without any problem

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,19 +5,18 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 
 	"snippitbox.chronoabi.com/internal/models"
+	"snippitbox.chronoabi.com/internal/validator"
 )
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -87,35 +86,40 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	// then create a form variable which will be a snippetCreate structure
 
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
 	// Validation for Title
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "Title cannot be empty !"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "Title cannot be more then 100 characters"
-	}
+	// if strings.TrimSpace(form.Title) == "" {
+	// 	form.FieldErrors["title"] = "Title cannot be empty !"
+	// } else if utf8.RuneCountInString(form.Title) > 100 {
+	// 	form.FieldErrors["title"] = "Title cannot be more then 100 characters"
+	// }
 
 	//content cannot be empty
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "Content field cannot be empty !"
-	}
+	// if strings.TrimSpace(form.Content) == "" {
+	// 	form.FieldErrors["content"] = "Content field cannot be empty !"
+	// }
 
 	// expires time can only be 1, 7 or 365 days
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "Expiry days can only be 1, 7 or 365 days "
+	// if expires != 1 && expires != 7 && expires != 365 {
+	// 	form.FieldErrors["expires"] = "Expiry days can only be 1, 7 or 365 days "
+	//
+	// }
 
-	}
-	// render comes form the helper.go
-	if len(form.FieldErrors) > 0 {
+  // because we have a valicator middleware we be using that
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+
+if !form.Valid() {
 		data := app.newTemplateDate(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
-		// fmt.Fprint(w, form.FieldErrors)
 		return
 	}
 
