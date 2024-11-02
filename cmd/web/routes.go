73f6d06fa2c6af7950dd9d1886347005471e68e5
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2024-11-02 17:20:00 +0545

    User Authorization

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,22 +15,30 @@ func (app *application) routes() http.Handler {
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
-
+	//Unprotected routes will use the normal dynamic middleware
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
-//snippets
+
+	//snippets
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
-	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
-//authentication
-  router.Handler(http.MethodGet,"/user/signup",dynamic.ThenFunc(app.useSignup))
-  router.Handler(http.MethodPost,"/user/signup",dynamic.ThenFunc(app.useSignupPost))
-  router.Handler(http.MethodGet,"/user/login",dynamic.ThenFunc(app.userLogin))
-  router.Handler(http.MethodPost,"/user/login",dynamic.ThenFunc(app.userLoginPost))
-  router.Handler(http.MethodPost,"/user/logout",dynamic.ThenFunc(app.userLogOutPost))
+
+	//authentication
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.useSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.useSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
 
 
+	//For protected ones we will first add the requireAuthntication middleware in dynamic
+	protected := dynamic.Append(app.requireAuthentication)
+
+	//snippets
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
+
+	//authentication
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogOutPost))
 
 	// a convinent way of writing middlewares then just returning function inside a function.
 	// it shows chains more properly
